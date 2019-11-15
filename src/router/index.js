927b326293f505afe95cb0afb6f596ca84e52767
wzy1997 <wzy1997>
2019-11-15 17:27:18 +0800

    前后台node.js+element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import ListArticle from '../views/ListArticle'
+import CreateArticle from '../views/CreateArticle'
+import EditArticle from '../views/EditArticle'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'list-article',
+    redirect:'/articles/index'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/articles/index',
+    name: 'list-article',
+    component:ListArticle
+  },
+  {
+    path: '/articles/create',
+    name: 'create-article',
+    component:CreateArticle
+  },
+  {
+    path: '/articles/:id/edit',
+    name: 'edit-article',
+    component:EditArticle
+  },
+  
 ]
 
 const router = new VueRouter({
   routes
+  
 })
 
 export default router
+
+
